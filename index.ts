04358e3f8037e59aa0463f92aa62987c105deb46
Muhammad Maarij <muhammadmaarij72gmail.com>
2023-07-17 10:02:20 +0500

    first commit

--- index.ts
+++ index.ts
@@ -29,5 +29,15 @@ class Calculator{
 }
 
 const Calculator1 = new Calculator();
-const add = Calculator1.exponent(12, 2);
-console.log(add)
+const add = Calculator1.add(12, 2);
+const sub = Calculator1.subtract(12, 2);
+const mul = Calculator1.multiply(12, 2);
+const divide = Calculator1.divide(12, 2);
+const exp = Calculator1.exponent(12, 2);
+const root = Calculator1.nthRoot(12, 2);
+console.log("Add: ",add)
+console.log("Subtract: ",sub)
+console.log("Multiply: ",mul)
+console.log("Divide: ",divide)
+console.log("Exponent: ",exp)
+console.log("Root: ",root)
